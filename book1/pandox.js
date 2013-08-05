69207c562f25504a150904c04c785cd6276ba127
Matheus Messora <matheus.messora.vpn@gmail.com>
2013-08-05 16:40:04 -0300

    JS

--- book1/pandox.js
+++ book1/pandox.js
@@ -6,18 +6,18 @@
  * @param myLesson - Lesson (practice, homework, text, grammar)
  */
 function eitchaLele(myUnit, myLesson){
-//    $.ajax({
-//        type: "POST",
-//        url: "http://www.ingleson.com.br/class/Book1Grammar1.aspx/Unlock",
-//        data: "{ _unit: '" + myUnit + "', _lesson:'" + myLesson + "'}",
-//        contentType: "application/json; charset=utf-8",
-//        dataType: "json",
-//        success: function (msg) {
-//
-//            var ar = msg.d.toString().split("|");
-//            parent.liberar('Unit ' + ar[1], ar[0], ar[2]);
-//        }
-//    });
+    $.ajax({
+        type: "POST",
+        url: "http://www.ingleson.com.br/class/Book1Grammar1.aspx/Unlock",
+        data: "{ _unit: '" + myUnit + "', _lesson:'" + myLesson + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+
+            var ar = msg.d.toString().split("|");
+            parent.liberar('Unit ' + ar[1], ar[0], ar[2]);
+        }
+    });
 
 }
 
